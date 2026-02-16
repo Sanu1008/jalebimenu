beca81c5f8f6985c0123fd61f46e64c82e7519b6
Sanu Muhammed <programmer@aljazira.com>
2026-02-16 12:07:03 +0300

    New MYSQLAddedport

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,             // e.g., containers-us-west-123.railway.app
-  user: process.env.MYSQL_USER,             // e.g., root
-  password: process.env.MYSQL_PASSWORD,     // your password
+  host: process.env.MYSQLHOST,             // e.g., containers-us-west-123.railway.app
+  user: process.env.MYSQLUSER,             // e.g., root
+  password: process.env.MYSQLPASSWORD,     // your password
   database: process.env.MYSQL_DATABASE,     // your database name
-  port: Number(process.env.MYSQL_PORT)      // make sure this is a number
+  port: Number(process.env.MYSQLPORT)      // make sure this is a number
 });
 
 module.exports = pool;
