5630b2f5aa1ae1aa1beb5ef738b15e599ed5e09e
Sanu Muhammed <programmer@aljazira.com>
2026-02-16 12:29:53 +0300

    New MYSQL

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,       // MUST match Railway plugin variable
+  host: process.env.MYSQL_HOST,        // Railway’s exact variable name
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
